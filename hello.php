aed0fb2053517d19da6ccb3759ba785c72d47247
Mo <moh19951@outlook.com>
2019-11-21 12:06:47 +0100

    Uitwerking van de exercise

--- hello.php
+++ hello.php
@@ -1,1 +1,11 @@
 <?php echo 'Hello World';
+
+print("Hallo wereld");
+print('Hallo wereld');
+print "Hallo wereld";
+print 'Hallo wereld';
+
+echo("Hallo wereld");
+echo('Hallo wereld');
+echo "Hallo wereld";
+echo 'Hallo wereld';
